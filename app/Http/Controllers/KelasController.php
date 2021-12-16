2db1a7a530a6cc39a5d11993b6fb0c5de938aa32
emRival <em.rival282@gmail.com>
2021-12-16 18:39:52 +0700

    first

--- app/Http/Controllers/KelasController.php
+++ app/Http/Controllers/KelasController.php
@@ -17,9 +17,8 @@ class KelasController extends Controller
     public function index(Request $request)
     {
       
-        $keyword = $request->cari;
-        $kelas = Kelas::where('nama_kelas', 'like', "%" . $keyword . "%")->paginate(4);
-        return view('jurusan.index', compact('kelas'));
+   
+        return view('landing', compact('kelas'));
     }
 
     /**
