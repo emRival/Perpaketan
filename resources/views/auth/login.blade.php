ce2a536873a2d56d33d5affd5986d98a4d2bfbda
emRival <em.rival282@gmail.com>
2021-12-16 13:32:38 +0700

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,72 +2,58 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+    <div class="position-absolute top-50 start-50 translate-middle" style="margin-top: 40px!important;">
+        <div class="card shadow p-3 mb-5 bg-white " style="margin-bottom: 70px !important; border-radius: 20px; ">
+            <div class="shadow  mb-3 bg-white" style="background-color: #0d6efd !important; border-radius: 15px;">
+                
+                
+                    <h2 class="text-center mt-2" style="color: white; font-size: 40px;text-shadow: 2px 2px; ">LOGIN</h2>
+                
+            </div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+            <div class="container">
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                    <div class="form-group w-70">
+                        <label for="email">{{ ('E-Mail') }}</label>
+                        <div >
+                            <input style="outline: none;border-radius: 20px;" id="email" placeholder="email" type="email" class="form-control w-100 @error('email') is-invalid @enderror shadow  mb-3 bg-white" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
                         </div>
+                    </div>
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                    <div class="form-group w-70">
+                        <label for="password" class="">{{ ('Password') }}</label>
+                        <div>
+                            <input style="outline:none; border-radius: 20px;" id="password" placeholder="Password" type="password" class="form-control @error('password') is-invalid @enderror shadow  mb-3 bg-white " name="password" required autocomplete="current-password">
+                            @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
                         </div>
+                    </div>
 
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
+                    <div class="form-group">
+                        <div class="text-center mt-4">
+                            <button type="submit" style="border-radius: 10px;" class="btn btn-primary ">
+                                {{ __('Login') }}
+                            </button>
                         </div>
+                    </div>
+                </form>
 
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
             </div>
         </div>
     </div>
 </div>
+
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="1" d="M0,288L288,160L576,320L864,64L1152,64L1440,0L1440,320L1152,320L864,320L576,320L288,320L0,320Z"></path></svg>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="1" d="M0,288L288,160L576,320L864,64L1152,64L1440,0L1440,0L1152,0L864,0L576,0L288,0L0,0Z"></path></svg>
+
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Paket IDN</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" >
 
     <link rel="stylesheet" href="{{asset('atlantis/assets/css/bootstrap.min.css')}}">
     <link rel="stylesheet" href="{{asset('atlantis/assets/css/atlantis.min.css')}}">
@@ -415,7 +415,7 @@
     @livewireScripts
 </head>
 
-<body>
+<body class="m-0 h-100">
     <nav class="navbar navbar-light bg-light" style="background-color: blue !important;">
         <a class="navbar-brand">Navbar</a>
 
